368af0057a104958a736fcf7f8926f5d1d14f1ae
artizin9 <reis08897@gmail.com>
2025-03-13 18:21:37 -0300

    the system settings have been. Everything is correct!

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import { routes } from './router'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider routes={routes} />
   </StrictMode>,
 )
---
+++ src/router.jsx
@@ -0,0 +1,9 @@
+import { createBrowserRouter } from "react-router-dom";
+
+export const routes = createBrowserRouter([{
+    path: '/',
+    children: [{
+        //  paht: '/auth/register',
+        //  element: <Register />
+    }]
+}]);
